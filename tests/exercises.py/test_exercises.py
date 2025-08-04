e570c1580fc2e241561e42252e2e5231093c4f74
Кирилл Панов <lliril_05@list.ru>
2025-08-04 12:52:27 +0300

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Создание задания

---
+++ tests/exercises.py/test_exercises.py
@@ -0,0 +1,31 @@
+from http import HTTPStatus
+import pytest
+
+from clients.exercises.exercises_client import ExercisesClient
+from clients.exercises.exercises_schema import (
+    CreateExerciseRequestSchema,
+    ExerciseResponseSchema,
+)
+from fixtures.courses import CourseFixture
+from tools.assertions.base import assert_status_code
+from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.schema import validate_json_schema
+
+
+@pytest.mark.regression
+@pytest.mark.exercises
+class TestExercises:
+
+    def test_create_exercise(
+        self, exercises_client: ExercisesClient, function_course: CourseFixture
+    ):
+        """Тест создания упражнения."""
+
+        request = CreateExerciseRequestSchema(course_id=function_course.course_id)
+        response = exercises_client.create_exercise_api(request)
+        response_data = ExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_exercise_response(response_data, request)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
---
+++ tools/assertions/exercises.py
@@ -0,0 +1,52 @@
+from clients.exercises.exercises_schema import (
+    ExerciseSchema,
+    ExerciseResponseSchema,
+    CreateExerciseRequestSchema,
+    GetExercisesResponseSchema,
+    UpdateExerciseRequestSchema,
+    GetExercisesQuerySchema,
+)
+from tools.assertions.base import assert_equal
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Проверка соответствия данных упражнения.
+
+    Args:
+        actual (ExerciseSchema): Текущие данные упражнения.
+        expected (ExerciseSchema): Ожидаемые данные упражнения.
+
+    Raises:
+        AssertionError: Если данные не совпадают.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_create_exercise_response(
+    response: ExerciseResponseSchema,
+    request: CreateExerciseRequestSchema,
+):
+    """
+    Проверяет, что при создании упражнения данные в ответе соответствуют ожиданиям.
+
+    Args:
+        response (ExerciseResponseSchema): Ответ сервера после создания упражнения.
+        request (CreateExerciseRequestSchema): Отправленные данные для создания.
+
+    Raises:
+        AssertionError: Если данные не совпадают.
+    """
+    for field_name in request.model_dump().keys():
+        assert_equal(
+            getattr(response.exercise, field_name),
+            getattr(request, field_name),
+            field_name,
+        )
